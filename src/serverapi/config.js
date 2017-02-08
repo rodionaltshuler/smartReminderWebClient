9ab74031a1ccc394548aab02020c034fad3e7bd9
Rodion Altshuler <rodion.altshuler@gmail.com>
2017-02-08 13:51:18 +0200

    Make API server configurable via process.env.CONFIG

--- src/serverapi/config.js
+++ src/serverapi/config.js
@@ -1,6 +1,26 @@
-const config = {
+let googleCloudConfig = {
   baseUrl: 'https://smart-reminder-us.appspot.com',
   baseApiUrl: 'https://smart-reminder-us.appspot.com/api/v1'
 };
+
+let localConfig = {
+  baseUrl: 'http://localhost:3001',
+  baseApiUrl: 'http://localhost:3001/api/v1'
+};
+
+let configs = {
+  google: googleCloudConfig,
+  local: localConfig
+};
+
+let config;
+
+if (process.env.CONFIG && configs[process.env.CONFIG]) {
+  config = configs[process.env.CONFIG];
+  console.log("Using config %s", JSON.stringify(configs[process.env.CONFIG]));
+} else {
+  config = configs.local;
+  console.log("no process.env.CONFIG variable defined or defined config doesn't exist - using local");
+}
 
 export default config;
